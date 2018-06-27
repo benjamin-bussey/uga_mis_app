078f3fb5ac30d6cff3261766a2713db0730eb3a8
Benjamin Bussey <bbussey23@gmail.com>
2018-06-27 00:47:19 -0400

    Updated homepage with information and set the boilerplate for screen navigation

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 A Flutter application delivering information about the UGA Management Information System major.
 
+##Functionality
+
+View general information about the major, professors, and courses.
+
 ## Getting Started
 
-For help getting started with Flutter, view our online
+For help getting started with Flutter, view the online
 [documentation](https://flutter.io/).
+
+To visit the MIS maipage, click[here](https://http://www.terry.uga.edu/academics/offices/management-information-systems)
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,67 +1,92 @@
 import 'package:flutter/material.dart';
 
+final _bulldog_red = Color(0xFFBA0C2F);
+
 class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return new HomeState();
+    return HomeState();
   }
 }
 
 class HomeState extends State<Home> {
-  final _bulldog_red = Color(0xFFBA0C2F);
-  int _counter = 0;
-
-  void _increaseCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('MIS App'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MIS - Terry College of Business'),
+        centerTitle: true,
         backgroundColor: _bulldog_red,
       ),
-      body: new Container(
-        child: new Column(
+      body: Container(
+        child: ListView(
           children: <Widget>[
-            new Center(
-              child: new Text(
-                "Display Area",
-                style: new TextStyle(
-                    color: Theme.of(context).primaryColor,
+            Padding(padding: const EdgeInsets.all(10.0)),
+            Center(
+              child: Text(
+                "About MIS",
+                style: TextStyle(
+                    color: Colors.black,
                     fontSize: 30.2,
                     fontWeight: FontWeight.w400),
               ),
             ),
-            new Expanded(
-                child: new Center(
-              child: new Text(
-                'Another Message',
-                style: new TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 50.0,
-                ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Management Information Systems (MIS) is a hands-on major which equips students with real-world skills that are in demand in the job market. Our program prepares students to lead IT-enabled change in organizations.',
+                    style: TextStyle(fontSize: 15.5),
+                  ),
+                  Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(
+                          0.0, 10.0, 0.0, 0.0)),
+                  Text(
+                    'The Terry MIS program provides the foundation for students to go in a technical direction (such as a programmer or database administrator), a non-technical direction (such as a business process analyst or project manager), or somewhere in the middle (such as a business intelligence or IT risk & security specialist).',
+                    style: TextStyle(fontSize: 15.5),
+                  ),
+                  Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0)),
+                  Image.asset(
+                    './images/mis_award.png',
+                    height: 175.0,
+                  ),
+                ],
               ),
-            )),
-            new Center(
-              child: new Text('$_counter'),
             ),
-            new MaterialButton(
-              color: Colors.blue,
-              onPressed: _increaseCounter,
-              child: new Text('Push Me!'),
-            )
           ],
         ),
       ),
-      bottomNavigationBar: new BottomNavigationBar(items: [
-        new BottomNavigationBarItem(icon: new Icon(Icons.home), title: new Text('Home'),),
-        new BottomNavigationBarItem(icon: new Icon(Icons.person), title: new Text('Faculty'),),
-        new BottomNavigationBarItem(icon: new Icon(Icons.book), title: new Text('Courses'),),
-      ], fixedColor: _bulldog_red,),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int option) => debugPrint('$option Pushed'),
+//TODO Figure out app navigation
+//            (int option) {
+//          var router = MaterialPageRoute(
+//            builder: (BuildContext context) => PageSelector(option),
+//          );
+//          Navigator.of(context).push(router);
+//        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text(
+              'Faculty',
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            title: Text('Courses'),
+          ),
+        ],
+        fixedColor: _bulldog_red,
+      ),
     );
   }
 }
